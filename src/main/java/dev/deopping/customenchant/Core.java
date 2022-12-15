1056ee67c20b0fb471430cb700a591864b0304a3
Mantice <79672154+DeOpping@users.noreply.github.com>
2022-12-15 18:23:32 -0500

    Commit #1 - 12/15/22

--- src/main/java/dev/deopping/customenchant/Core.java
+++ src/main/java/dev/deopping/customenchant/Core.java
@@ -1,2 +1,56 @@
-package dev.deopping.customenchant;public class Core {
+package dev.deopping.customenchant;
+
+import dev.deopping.customenchant.enchants.EXPFinder;
+import dev.drawethree.ultraprisoncore.UltraPrisonCore;
+import dev.drawethree.ultraprisoncore.enchants.UltraPrisonEnchants;
+import dev.drawethree.ultraprisoncore.enchants.api.UltraPrisonEnchantsAPI;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Core extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        initialize();
+
+        // Register Enchants
+        api.registerEnchant(new EXPFinder(this, 101));
+
+        getLogger().info("CustomEnchant " + getDescription().getVersion() + " enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("CustomEnchant " + getDescription().getVersion() + " disabled!");
+    }
+
+
+
+    private void initialize() {
+        if (getServer().getPluginManager().getPlugin("UltraPrisonCore") == null) {
+            getLogger().info("This plugin requires UltraPrisonCore!");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        this.upc = UltraPrisonCore.getInstance();
+
+        if (!upc.isModuleEnabled(UltraPrisonEnchants.MODULE_NAME)) {
+            getLogger().info("UltraPrisonCore Enchants module is disabled!");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        api = upc.getEnchants().getApi();
+    }
+
+
+
+    private UltraPrisonCore upc;
+    public UltraPrisonCore getUPC() { return upc; }
+
+    private UltraPrisonEnchantsAPI api;
+    public UltraPrisonEnchantsAPI getApi() { return api; }
+
+
+
 }
--- src/main/java/dev/deopping/customenchant/enchants/EXPFinder.java
+++ src/main/java/dev/deopping/customenchant/enchants/EXPFinder.java
@@ -1,15 +1,25 @@
 package dev.deopping.customenchant.enchants;
 
+import dev.deopping.customenchant.Core;
+import dev.drawethree.ultraprisoncore.UltraPrisonCore;
 import dev.drawethree.ultraprisoncore.enchants.UltraPrisonEnchants;
 import dev.drawethree.ultraprisoncore.enchants.model.UltraPrisonEnchantment;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
-public class BasicEnchant extends UltraPrisonEnchantment {
+import java.util.concurrent.ThreadLocalRandom;
 
-    public BasicEnchant(UltraPrisonEnchants ultraPrisonEnchants, int i) {
-        super(ultraPrisonEnchants, i);
+public class EXPFinder extends UltraPrisonEnchantment {
+
+    private final UltraPrisonCore upc;
+
+    public EXPFinder(Core core, int i) {
+        super(UltraPrisonEnchants.getInstance(), i);
+        this.upc = core.getUPC();
     }
 
 
@@ -18,25 +28,38 @@ public class BasicEnchant extends UltraPrisonEnchantment {
     public String getAuthor() {
         return "Mantice";
     }
-    
-
-
-    @Override
-    public void onEquip(Player player, ItemStack itemStack, int i) {
-
-    }
 
 
 
     @Override
-    public void onUnequip(Player player, ItemStack itemStack, int i) {
+    public void onEquip(Player player, ItemStack itemStack, int level) {}
 
-    }
+    @Override
+    public void onUnequip(Player player, ItemStack itemStack, int level) {}
 
 
 
     @Override
-    public void onBlockBreak(BlockBreakEvent blockBreakEvent, int i) {
+    public void onBlockBreak(BlockBreakEvent event, int level) {
+
+        Player player = event.getPlayer();
+        YamlConfiguration cfg = upc.getEnchants().getEnchantsConfig().getYamlConfig();
+        String path = "enchants.101.";
+
+        double chance = cfg.getDouble(path + "Chance");
+        if (chance * level < ThreadLocalRandom.current().nextDouble(100)) return;
+
+        String maxString = cfg.getString(path + "Max-Exp").replace("{LEVEL}", level + "");
+        String minString = cfg.getString(path + "Min-Exp").replace("{LEVEL}", level + "");
+
+        Expression maxExpression = new ExpressionBuilder(maxString).build();
+        Expression minExpression = new ExpressionBuilder(minString).build();
+
+        double max = maxExpression.evaluate();
+        double min = minExpression.evaluate();
+
+        double random = ThreadLocalRandom.current().nextDouble(min, max);
+        player.giveExp((int) random);
 
     }
 
@@ -44,7 +67,8 @@ public class BasicEnchant extends UltraPrisonEnchantment {
 
     @Override
     public double getChanceToTrigger(int i) {
-        return 0;
+        YamlConfiguration cfg = upc.getEnchants().getEnchantsConfig().getYamlConfig();
+        return cfg.getDouble("enchants.101.Chance");
     }
 
 
